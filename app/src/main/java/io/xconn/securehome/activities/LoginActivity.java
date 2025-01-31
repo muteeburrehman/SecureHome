0e5699a5eeb2d50cc2e1d31d8e8c394aa943a847
muteeburrehman <muteeb285@gmail.com>
2025-01-31 15:34:43 +0500

    updated screen designs (login, registration)

---
+++ app/src/main/java/io/xconn/securehome/activities/LoginActivity.java
@@ -0,0 +1,17 @@
+package io.xconn.securehome.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import io.xconn.securehome.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
